f1dd6100c8a43fc6efff7b0cb8a4473ced4c035e
Jonatas Laguna <jonataslaguna.js@gmail.com>
2023-10-19 11:27:12 -0300

    tests sales model

---
+++ backend/tests/unit/Mocks/sales.mock.js
@@ -0,0 +1,33 @@
+const salesFromDBAndSalesFromModel = [
+  {
+    saleId: 1,
+    date: '2023-10-19T17:11:46.000Z',
+    productId: 1,
+    quantity: 5,
+  },
+  {
+    saleId: 1,
+    date: '2023-10-19T17:11:46.000Z',
+    productId: 2,
+    quantity: 10,
+  },
+  {
+    saleId: 2,
+    date: '2023-10-19T17:11:46.000Z',
+    productId: 3,
+    quantity: 15,
+  },
+];
+
+const saleId = [
+  {
+    date: '2023-10-19T17:11:46.000Z',
+    productId: 3,
+    quantity: 15,
+  },
+];
+
+module.exports = {
+  salesFromDBAndSalesFromModel,
+  saleId,
+};
---
+++ backend/tests/unit/models/sales.model.test.js
@@ -0,0 +1,36 @@
+const { expect } = require('chai');
+const sinon = require('sinon');
+const connection = require('../../../src/models/connection');
+const { salesModel } = require('../../../src/models');
+
+const {
+  salesFromDBAndSalesFromModel, 
+  saleId,
+} = require('../Mocks/sales.mock');
+
+describe('SALES MODEL', function () {
+  afterEach(function () {
+    sinon.restore();
+  });
+    
+  it('Test function findAll', async function () {
+    sinon.stub(connection, 'execute').resolves([salesFromDBAndSalesFromModel]);
+  
+    const sales = await salesModel.findAll();
+  
+    expect(sales).to.be.an('array');
+    expect(sales).to.have.lengthOf(3);
+    expect(sales).to.be.deep.equal(salesFromDBAndSalesFromModel);
+  });
+
+  it('Test function finById', async function () {
+    sinon.stub(connection, 'execute').resolves([saleId]);
+
+    const inputValue = 2;
+    const sales = await salesModel.findById(inputValue);
+
+    expect(sales).to.be.an('array');
+    expect(sales).to.have.lengthOf(1);
+    expect(sales).to.be.deep.equal(saleId);
+  });
+});
